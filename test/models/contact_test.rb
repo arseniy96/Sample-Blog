798a292915163db246588348af3aa1d027152934
arseniy96 <arsenzhar@gmail.com>
2016-03-03 20:01:28 +0300

    Add test model Contact (empty?)

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "contact attributes must not be empty" do
+    contact = Contact.new
+    assert contact.invalid?
+    assert contact.errors[:email].any?
+    assert contact.errors[:email].any?
+  end
+
 end
