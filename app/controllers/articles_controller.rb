b564b69326535375d22407f787cbea568bf8c29a
arseniy96 <arsenzhar@gmail.com>
2016-03-11 21:14:14 +0300

    Add caption wrtiter to Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.username = current_user.email
     if @article.save
       redirect_to @article
     else
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20160311102057) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
+    t.string   "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
