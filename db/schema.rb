e7b41e151f05972ee99fc2985647df33fc487cd7
arseniy96 <arsenzhar@gmail.com>
2016-03-14 23:28:39 +0300

    Add column username to articles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314170411) do
+ActiveRecord::Schema.define(version: 20160314202609) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "username"
   end
 
   create_table "comments", force: :cascade do |t|
