015f2d5e86d5bfda204ab5a484e1358911221374
arseniy96 <arsenzhar@gmail.com>
2016-03-15 00:02:41 +0300

    Add commentator to comments to article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,18 @@
 class CommentsController < ApplicationController
 
   def create
-    @article = Article.find(params[:article_id])
-    @article.comments.create(comment_params)
+    @art = Article.find(params[:article_id])
+    @article = @art.comments.new(comment_params)
+    @article.author = current_user.username
+    @article.save
 
-    redirect_to article_path(@article)
+    redirect_to article_path(@art)
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:author, :body)
+    params.require(:comment).permit(:body)
   end
 
 end
