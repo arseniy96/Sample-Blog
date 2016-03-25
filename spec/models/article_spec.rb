8bb1e9d791a4c6cf505574451cf88c7b5fa99a1c
arseniy96 <arsenzhar@gmail.com>
2016-03-25 10:00:29 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,6 +6,11 @@ describe Article do
     it { should validate_presence_of :text }
   end
 
+  describe "validations for length" do
+    it { should validate_length_of(:title).is_at_most(140) }
+    it { should validate_length_of(:text).is_at_most(4000) }
+  end
+
   describe "associations" do
     it { should have_many :comments }
   end
